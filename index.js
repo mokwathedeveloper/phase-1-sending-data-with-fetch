cdb18d7bb72a826ef4a18396373103f2f7286e46
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-10-13 14:49:39 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your code here
+const dogUrl="http://localhost:3000/dogs"
+
+const catUrl="http://localhost:3000/cats"
+
+const userUrl="http://localhost:3000/users" 
+
+const robortUrl="http://localhost:3000/robots"  
+
+ const nameInput=document.querySelector("name") 
+
+ function configureForm =() => {
+     const form=document.querySelector("form") 
+     )
